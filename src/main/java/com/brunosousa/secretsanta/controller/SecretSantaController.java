daa9ba933004650d2cd755e7a547e9a410af2693
brunoval <bhsousa@gmail.com>
2023-12-07 19:28:10 -0300

    simple draw with received names

--- src/main/java/com/brunosousa/secretsanta/controller/SecretSantaController.java
+++ src/main/java/com/brunosousa/secretsanta/controller/SecretSantaController.java
@@ -3,9 +3,10 @@ package com.brunosousa.secretsanta.controller;
 import com.brunosousa.secretsanta.service.SecretSantaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(("/api/v1/secretsanta"))
@@ -17,7 +18,13 @@ public class SecretSantaController {
     @GetMapping("/")
     public ResponseEntity<String> draw() {
 
-        String response = service.draw();
+        String response = service.draw(Arrays.asList("Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Helen", "Igor", "Jack"));
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/draw")
+    public ResponseEntity<String> drawNames(@RequestBody List<String> names) {
+        String response = service.draw(names);
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/brunosousa/secretsanta/service/SecretSantaService.java
+++ src/main/java/com/brunosousa/secretsanta/service/SecretSantaService.java
@@ -2,17 +2,13 @@ package com.brunosousa.secretsanta.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class SecretSantaService {
 
-    public String draw() {
-
-        List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Helen", "Igor", "Jack");
+    public String draw(List<String> names) {
 
         Collections.shuffle(names);
         System.out.println(names);
