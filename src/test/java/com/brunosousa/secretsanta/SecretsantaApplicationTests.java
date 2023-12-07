afc4acddca1afe0bb75546304a4438688573ce4d
brunoval <bhsousa@gmail.com>
2023-12-07 18:48:48 -0300

    Initial commit

---
+++ src/test/java/com/brunosousa/secretsanta/SecretsantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunosousa.secretsanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretsantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
